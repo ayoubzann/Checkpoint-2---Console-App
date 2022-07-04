ea53fc2113baf654a51bb043b77471b03ef14da1
ayoubzann <ayoubzann@gmail.com>
2022-07-04 08:48:44 +0200

    Add project files.

---
+++ Checkpoint 2 - Console App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Checkpoint_2___Console_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
