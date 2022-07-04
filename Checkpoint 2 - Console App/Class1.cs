7b4d1c0434535e6c351d7134df8f2580fb5af2ef
ayoubzann <ayoubzann@gmail.com>
2022-07-04 08:55:40 +0200

    Added three classes and properties to each.

---
+++ Checkpoint 2 - Console App/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age 
+        {
+            get { return age; } 
+            set { age = value; } 
+        }
+    }
+
+    public class Employee : Person
+    {
+        public string hireDate;
+
+        public string HireDate
+        {
+            get { return hireDate; }
+            set { hireDate = value; }
+        }
+    }
+
+    class Company
+    {
+        public string companyName
+        {
+            get { return companyName; }
+            set { companyName = value; }
+        }
+    }
+}
